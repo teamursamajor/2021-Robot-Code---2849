84578133941f1430819aa5e06598dc3016181dfc
teamursamajor <teamursamajor@gmail.com>
2022-01-29 08:40:18 -0800

    Created skeletons for these. toodles!

---
+++ src/main/java/frc/robot/subsystems/CatapultSubsystem.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+public class CatapultSubsystem {
+    final Spark launcher = new Spark(5);
+    public CatapultSubsystem() {}
+
+    //@Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+    }
+  
+    //@Override
+    public void simulationPeriodic() {
+      // This method w
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
