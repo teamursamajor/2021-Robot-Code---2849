3dfb7247db69f3c2b48babddeeedbd09a49ce520
teamursamajor <teamursamajor@gmail.com>
2022-02-05 10:56:32 -0800

    Adds code to test color sensor

--- src/main/java/frc/robot/ColorRGB.java
+++ src/main/java/frc/robot/ColorRGB.java
@@ -4,17 +4,16 @@ import edu.wpi.first.wpilibj.util.Color;
 
 public final class ColorRGB {
 
-    public final int RED, GREEN, BLUE;
+  public final int RED, GREEN, BLUE;
 
-    public ColorRGB(Color c) {
-        this.RED = (int)(c.red*255);
-        this.GREEN = (int)(c.green*255);
-        this.BLUE = (int)(c.blue*255);
-    }
+  public ColorRGB(Color c) {
+    this.RED = (int) (c.red * 255);
+    this.GREEN = (int) (c.green * 255);
+    this.BLUE = (int) (c.blue * 255);
+  }
 
-    @Override
-    public String toString() {
-        return String.format("%d:%d:%d", RED, GREEN, BLUE);
-    }
-
+  @Override
+  public String toString() {
+    return String.format("%d:%d:%d", RED, GREEN, BLUE);
+  }
 }
