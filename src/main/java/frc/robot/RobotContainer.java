9625cf7bfc2bb2181f761b320b9d81970ea1001e
teamursamajor <teamursamajor@gmail.com>
2022-02-07 16:12:57 -0800

    updated

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.commands.DriveCommand;
-import frc.robot.commands.ClimbCommand;
+//import frc.robot.commands.ClimbCommand;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import static frc.robot.Constants.XBOXCONTROLLER;
