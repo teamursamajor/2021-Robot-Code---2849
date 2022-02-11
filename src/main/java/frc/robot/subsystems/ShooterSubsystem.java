8cd55f350ab6d7f1109bc128800d35becdcae6a5
teamursamajor <teamursamajor@gmail.com>
2022-02-11 15:59:02 -0500

    cool

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,7 +4,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterSubsystem extends SubsystemBase {
-    final Spark launcher = new Spark(5);
+    public final Spark launcher = new Spark(5);
     public ShooterSubsystem() {}
 
     //@Override
