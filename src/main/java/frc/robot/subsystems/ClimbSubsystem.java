3c9989db05ef8c17fc046d18700b9c99a0f04d46
teamursamajor <teamursamajor@gmail.com>
2022-02-12 13:50:37 -0500

    spotless fix?

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,13 +1,10 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-import com.ctre.phoenix.sensors.CANCoder;
-
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class ClimbSubsystem extends SubsystemBase {
   // private final ClimbSubsystem m_ClimbSubsystem;
   public Spark climber = new Spark(3);
@@ -18,20 +15,14 @@ public class ClimbSubsystem extends SubsystemBase {
     setName("Climb");
   }
 
-    //@Override
-    public void periodic() {
-        
-    }
+  // @Override
+  public void periodic() {}
 
-    //@Override
-    public void SimulationPeriodic() {
-        
-    }
-    public void moterMove(double speed){
-        climbOne.set(TalonFXControlMode.PercentOutput, speed);
-        climbOne.set(TalonFXControlMode.PercentOutput, speed);
-    }
+  // @Override
+  public void SimulationPeriodic() {}
 
-    
-
+  public void moterMove(double speed) {
+    climbOne.set(TalonFXControlMode.PercentOutput, speed);
+    climbOne.set(TalonFXControlMode.PercentOutput, speed);
+  }
 }
