d532e239314a083ae963bf8a67b58b1fbe64525c
teamursamajor <teamursamajor@gmail.com>
2022-02-19 11:30:42 -0800

    merges climb command

--- src/main/java/frc/robot/commands/ClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -15,17 +15,6 @@ import frc.robot.subsystems.ClimbSubsystem;
  *
  * <p>Will say on Driver Station "Ready to go."
  */
-<<<<<<< HEAD:src/main/java/frc/robot/commands/ClimbCommand.Java
-=======
-
-/**
- * Going to add a comment when sensors are recieved to allow for the driver to know if: 1.) an arm
- * is on the robot (left arm and right arm) 2.) both arms are on the robot 3.) the robot is ready to
- * move
- *
- * <p>Will say on Driver Station "Ready to go."
- */
->>>>>>> cf5a06fe2d98a9dcc01a4696c6fc6547c487896e:src/main/java/frc/robot/commands/ClimbCommand.java
 public class ClimbCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private static int num = 0;
@@ -56,7 +45,6 @@ public class ClimbCommand extends CommandBase {
     log(CLIMB_SUBSYSTEM, "Execute", INFO);
   }
 
-<<<<<<< HEAD:src/main/java/frc/robot/commands/ClimbCommand.Java
     }
   }
 
@@ -66,14 +54,6 @@ public class ClimbCommand extends CommandBase {
   }
 
   @Override
-=======
-  @Override
-  public void end(boolean interrupted) {
-    System.out.println("Done");
-  }
-
-  @Override
->>>>>>> cf5a06fe2d98a9dcc01a4696c6fc6547c487896e:src/main/java/frc/robot/commands/ClimbCommand.java
   public boolean isFinished() {
     return false;
   }
