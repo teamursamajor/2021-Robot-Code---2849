0971f3e4f802912bc45565ae3e6dd145af479de8
Emilyn Heikell <aheike@inst.hcpss.org>
2022-02-23 17:08:27 -0500

    Alr Ig

--- src/main/java/frc/robot/DriverUI.java
+++ src/main/java/frc/robot/DriverUI.java
@@ -5,33 +5,11 @@ import java.util.function.Supplier;
 
 import static frc.robot.Constants.*;
 
-import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 
 public final class DriverUI {
 
-    private final ShuffleboardTab CONTROLS_TAB = Shuffleboard.getTab("Controls");
-    private final ShuffleboardTab CONSOLE_TAB = Shuffleboard.getTab("Console");
-    private final ShuffleboardTab HOME_TAB = Shuffleboard.getTab("Home");
-
     public DriverUI() {
-        tabInit();
-    }
-
-    private void tabInit() {
-        homeTabInit();
-        consoleTabInit();
-        controlsTabInit();
-    }
-
-    private void homeTabInit() {
-        HOME_TAB.addString("Team Color", () -> TEAM_COLOR);
-    }
-
-    private void consoleTabInit() {
-        CONSOLE_TAB.addStringArray("Log", () -> getLogs());
-    }
-
-    private void controlsTabInit() {
-
+        
     }
 }
